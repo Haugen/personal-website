0adc06bdac8f29405f92774f30aa8a4a3ebdeca3
Haugen <tobiashaugen@gmail.com>
2022-12-16 13:29:17 +0100

    New font and fiddling with header design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["Roboto", "sans-serif"],
+        sans: ["Figtree", ...defaultTheme.fontFamily.sans],
       },
     },
   },
